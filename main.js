5b6355d9f76d8207267e4f440fe7f3abe22f9a77
kchandler44 <kellychandler624@gmail.com>
2023-11-01 18:53:19 -0600

    testing image

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const parent = contents.parentNode;
 parent.removeChild(contents);
 
 image = document.createElement("IMG")
-image.setAttribute("src", "./images/12.jpg")
+image.setAttribute("src", "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg")
 image.setAttribute("width", "500")
 image.setAttribute("alt", "picture")
 parent.appendChild(image)
