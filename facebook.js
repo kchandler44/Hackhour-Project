0c66037d216b1d5544ed768bafac8f932b4d7369
kchandler44 <kellychandler624@gmail.com>
2023-11-02 15:32:32 -0600

    final

--- facebook.js
+++ facebook.js
@@ -7,9 +7,7 @@ const array = [
   "https://assets3.thrillist.com/v1/image/3005227/1584x1630/scale;webp=auto;jpeg_quality=60.jpg",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNLBk2yuYgBqIADi0MIZ81h59tQvPy6vuBxFtvLmYhaC-pj0qm",
   "https://www.dailypaws.com/thmb/uOCuf9oXdXmWGv0LuiR8pb_pHU4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/kitten-pounce-158906394-2000-8d4fa56377264aefa98b34b09534bfb9.jpg",
-  "https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg",
-  "https://pixabay.com/photos/cat-flower-kitten-stone-pet-2536662/", 
-
+  "https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg", 
 ];
 
 const imageIndex = Math.floor(Math.random() * array.length);
