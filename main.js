cac43102c94b02ddf76ac8b18247273c67e06c33
kchandler44 <kellychandler624@gmail.com>
2023-11-01 18:40:25 -0600

    added images

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ const parent = contents.parentNode;
 parent.removeChild(contents);
 
 image = document.createElement("IMG")
-image.setAttribute("src", "./images/01.png") 
-parent.append(image)
+image.setAttribute("src", "./images/12.jpg")
+image.setAttribute("width", "500")
+image.setAttribute("alt", "picture")
+parent.appendChild(image)
